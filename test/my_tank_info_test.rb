afb4e7cdab3466d15420231f73209b6f3d96e902
Kyle Keesling <me@kylekeesling.com>
2021-09-14 19:43:57 -0400

    Fix test setup

--- test/my_tank_info_test.rb
+++ test/my_tank_info_test.rb
@@ -6,8 +6,4 @@ class MyTankInfoTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MyTankInfo::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
