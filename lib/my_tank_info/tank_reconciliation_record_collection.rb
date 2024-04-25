7f47c459852af3ef5190661774bb891752459ca6
Kyle Keesling <me@kylekeesling.com>
2024-04-25 15:33:05 -0400

    more recon tweaks

--- lib/my_tank_info/tank_reconciliation_record_collection.rb
+++ lib/my_tank_info/tank_reconciliation_record_collection.rb
@@ -14,8 +14,9 @@ module MyTankInfo
       )
     end
 
-    def initialize(data:, reconciliation_period:, auto_fill_needed: nil)
-      @data = data
+    def initialize(data:, reconciliation_period:)
+      # auto_fill_needed means the record is not ready to be displayed to the user yet
+      @data = data.select { _1.auto_fill_needed == false }
       @size = @data.size
       @reconciliation_period = reconciliation_period
 
@@ -25,21 +26,14 @@ module MyTankInfo
 
       @volume_uom = @data.first&.volume_uom
       @height_uom = @data.first&.height_uom
-
-      @auto_fill_needed = auto_fill_needed
     end
 
     def tanks
       @data.map { |record|
-        base_records = @data.select { _1.tank_number == record.tank_number }.sort_by(&:started_at)
         records =
-          if @auto_fill_needed.nil?
-            base_records
-          elsif @auto_fill_needed
-            base_records.select { _1.auto_fill_needed == true }
-          else
-            base_records.select { _1.auto_fill_needed == false }
-          end
+          @data
+            .select { _1.tank_number == record.tank_number }
+            .sort_by(&:started_at)
 
         summary =
           TankReconciliationRecordSummary.new(
